1252db4132538856dc1e2ecda66049bcc61462d4
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-01-19 12:12:11 +0100

    gg

--- src/Components/MusicPlayer.jsx
+++ src/Components/MusicPlayer.jsx
@@ -16,22 +16,22 @@ const MusicPlayer = () => {
                         <Row className="h-100">
                             <Col lg={10} className="offset-lg-2">
                                 <Row className="h-100 flex-column justify-content-center align-items-center">
-                                    <Col className="col-6 col-md-4 playerControls">
-                                        <div className="d-flex">
+                                    <Col xs={6} md={4} className="playerControls">
+                                        <div className="d-flex align-items-center">
                                             <a href="#">
-                                                <img src={shuffle} alt="shuffle" />
+                                                <img className="icon" src={shuffle} alt="shuffle" />
                                             </a>
                                             <a href="#">
-                                                <img src={prev} alt="prev" />
+                                                <img className="icon" src={prev} alt="prev" />
                                             </a>
                                             <a href="#">
-                                                <img src={play} alt="play" />
+                                                <img className="icon" src={play} alt="play" />
                                             </a>
                                             <a href="#">
-                                                <img src={next} alt="next" />
+                                                <img className="icon" src={next} alt="next" />
                                             </a>
                                             <a href="#">
-                                                <img src={repeat} alt="repeat" />
+                                                <img className="icon" src={repeat} alt="repeat" />
                                             </a>
                                         </div>
                                         <div className="progress mt-3">
