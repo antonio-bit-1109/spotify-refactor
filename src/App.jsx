f15b081e7ac540ad712acae2e6a0dd7572e4b002
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-01-19 10:26:12 +0100

    pronti

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import MainComponent from "./Components/MainComponent";
 
-function App() {
-    return <div></div>;
-}
+const App = () => {
+    return (
+        <>
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<MainComponent />} />
+                </Routes>
+            </BrowserRouter>
+        </>
+    );
+};
 
 export default App;
