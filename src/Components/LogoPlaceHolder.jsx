1e5c47c81eed21e8fc942d1676ffd7da55a79cb5
antonio-bit-1109 <143196020+antonio-bit-1109@users.noreply.github.com>
2024-01-21 13:13:38 +0100

    gg

--- src/Components/LogoPlaceHolder.jsx
+++ src/Components/LogoPlaceHolder.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useRef, useState } from "react";
 import logo from "../assets/logo/logo.png";
 import { Col, Container, Row } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
-import { CSSTransition } from "react-transition-group";
 
 const LogoPlaceHolder = () => {
     const navigate = useNavigate();
